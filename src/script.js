261fb9b4e7d1791ed3fdd939775eeaa5ea5f0ce3
edwardscamera <42257904+edwardscamera@users.noreply.github.com>
2020-09-25 18:46:15 -0400

    Download paper jars to custom named folder

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@
 const fs = require("fs");
 const http = require("follow-redirects").http;
 // Navbar Setup
-for(let c of document.querySelector('#Navbar').querySelector('ul').children) {
+for (let c of document.querySelector('#Navbar').querySelector('ul').children) {
     c.onclick = () => {
-        for(let c of document.querySelector('#Navbar').querySelector('ul').children) c.classList.remove('Selected');
+        for (let c of document.querySelector('#Navbar').querySelector('ul').children) c.classList.remove('Selected');
         c.classList.toggle('Selected');
         showTab(c.innerText);
         retrieveLocalServers();
@@ -13,18 +13,18 @@ for(let c of document.querySelector('#Navbar').querySelector('ul').children) {
 // Content Setup
 document.querySelector('#Content').style.marginLeft = document.querySelector('#Navbar').offsetWidth + 'px';
 // Function Declaration
-const removeChildren = parent => { while(parent.firstChild) parent.removeChild(parent.firstChild); }
+const removeChildren = parent => { while (parent.firstChild) parent.removeChild(parent.firstChild); }
 const showTab = tab => {
-    for(let t of document.querySelector('#Content').children) {
+    for (let t of document.querySelector('#Content').children) {
         t.style.display = 'none';
         if (t.id === tab) t.style.display = 'block';
     }
 }
 showTab('Servers');
 const getDirectories = source =>
-  fs.readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
+    fs.readdirSync(source, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
 const retrieveLocalServers = () => {
     document.querySelector('#Servers-DisplayText').innerText = 'Retrieving Local Servers...';
     removeChildren(document.querySelector('#Servers-List'));
@@ -72,11 +72,20 @@ const newServer = () => {
         document.querySelector('#NewServer-FetchVersion').style.display = 'block';
     });
     document.querySelector('#NewServer-JarDownload').onclick = () => {
-        let file = fs.createWriteStream(`${process.env.LOCALAPPDATA}\\MinecraftServerShell\\server.jar`);
-        let request = http.get(`http://papermc.io/api/v1/paper/${document.querySelector('#NewServer-VersionSelect').value}/${document.querySelector('#NewServer-BuildSelect').value.split(' ')[0]}/download`, response => {
-            response.pipe(file);
-            console.log(response);
+        let serverName = document.querySelector('#NewServer-ServerName').value.replace(/\/||\\||\||\|||./g, '')
+        fs.mkdir(`${process.env.LOCALAPPDATA}\\MinecraftServerShell\\Servers\\${serverName}`, err => {
+            if (err === null) {
+                try {
+                    let file = fs.createWriteStream(`${process.env.LOCALAPPDATA}\\MinecraftServerShell\\Servers\\${serverName}\\server.jar`);
+                    let request = http.get(`http://papermc.io/api/v1/paper/${document.querySelector('#NewServer-VersionSelect').value}/${document.querySelector('#NewServer-BuildSelect').value.split(' ')[0]}/download`, response => {
+                        response.pipe(file);
+                    });
+                } catch (error) {
+                    alert(`An error occurred downloading the server jar.\n\n${error}`)
+                }
+            } else {
+                alert(`An error occurred creating the folder.\n\n${error}`);
+            }
         });
-        //console.log(`http://papermc.io/api/v1/paper/${document.querySelector('#NewServer-VersionSelect').value}/${document.querySelector('#NewServer-BuildSelect').value.split(' ')[0]}/download`);
     };
 };
