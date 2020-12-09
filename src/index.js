1f66ad05be8eccc95f878313cc14519b7918104e
edwardscamera <42257904+edwardscamera@users.noreply.github.com>
2020-12-08 23:26:10 -0500

    Panel select component work

--- src/index.js
+++ src/index.js
@@ -2,14 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class PanelContainer extends React.Component {
+
+class PanelSwitcher extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttons: [
+                {
+                    display: "Console",
+                    panelName: "console",
+                },
+                {
+                    display: "Backups",
+                    panelName: "backups",
+                },
+            ],
+        }
+    }
     render() {
+        let options = [];
+        this.state.buttons.forEach(button => {
+            options.push(
+                <div className="PanelSwitcher_Option">
+                    {button.display}
+                </div>
+            );
+        });
         return (
-            <h1>test</h1>
+            <div className="PanelSwitcher_Container">{options}</div>
         );
     }
 }
+
+class PanelContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            panel: props.initPanel,
+        };
+    }
+    render() {
+        switch (this.state.panel) {
+            default:
+            case 'Servers':
+                return (
+                    <PanelSwitcher></PanelSwitcher>
+                );
+            case 'Backups':
+                return (
+                    <PanelSwitcher></PanelSwitcher>
+                );
+        }
+    }
+}
+
 ReactDOM.render(
-    <PanelContainer render="Servers"></PanelContainer>,
+    <PanelContainer initPanel="Servers"></PanelContainer>,
     document.getElementById('root')
 );
