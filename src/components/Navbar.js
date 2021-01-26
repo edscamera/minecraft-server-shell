813bd8482e994f0d427dc83617e4e1dc60bf2ca0
edwardscamera <42257904+edwardscamera@users.noreply.github.com>
2021-01-26 18:46:28 -0500

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import "./Navbar.css";
 
 class Navbar extends React.Component {
+    static option = null;
     constructor(props) {
         super(props);
         let options = [
@@ -12,13 +14,34 @@ class Navbar extends React.Component {
             options: options,
             selected: options[0],
         };
+        Navbar.option = this.state.selected;
     }
-    render() {
-        return (
-            <div className="Navbar_Container">
-                test
+    render = () => {
+        let options = this.state.options.map(op =>
+            <div
+                key={op}
+                className={this.state.selected === op ? "Navbar_Item_Selected" : "Navbar_Item"}
+                onClick={() => this.setOption(op)}
+            >
+                {op}
             </div>
         );
+        let j = <div className="Navbar_Container">
+            {options}
+        </div>
+        return (
+            <div>
+                {j}
+                <div style={{ marginLeft: "250px" }}>
+                    test
+                </div>
+            </div>
+
+        );
+    }
+    setOption = (op) => {
+        this.setState({ selected: op });
+        Navbar.option = op;
     }
 }
 
