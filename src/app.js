f21823e80416e2e0bb46ef5a8bfdb14343cdd3ec
edwardscamera <42257904+edwardscamera@users.noreply.github.com>
2021-02-07 13:42:06 -0500

    Resizing image, writing files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const os = require("os");
 const path = require("path");
 const dialog = require("electron").remote.dialog;
+const resizeImg = require("resize-img");
 
 const DIR = {
     HOME: path.join(os.homedir(), "./MinecraftServerShell/"),
@@ -44,12 +45,72 @@ fs.watch(DIR.SERVERS, () => updateServerList(DIR.SERVERS));
 updateServerList(DIR.SERVERS);
 
 const createServer = () => {
+    const serverName = document.querySelector("#AddServer_Name").value;
+
+    if (/\/|\\|:|\*|\?|<|>|\|/g.test(serverName)) {
+        dialog.showMessageBoxSync(null, {
+            type: "error",
+            title: "Minecraft Server Shell",
+            message: "A server name can't contain any of the following characters: \n\n/ \\ : * ? < > |",
+        });
+        return;
+    }
+    if ([null, undefined, ""].includes(serverName) || serverName.replace(/\s/g, "").length === 0) {
+        dialog.showMessageBoxSync(null, {
+            type: "error",
+            title: "Minecraft Server Shell",
+            message: "A server name can't be empty.",
+        });
+        return;
+    }
+
     if (dialog.showMessageBoxSync(null, {
         type: "question",
         title: "Minecraft Server Shell",
         buttons: ["Yes", "Cancel"],
-        message: `Do you really want to create the server "${document.querySelector("#AddServer_Name").value}" with these settings? The settings can be changed afterwards.`,
+        message: `Do you really want to create the server "${serverName}" with these settings? The settings can be changed afterwards.`,
     }) === 1) return;
+
+    try {
+        fs.mkdirSync(path.join(DIR.SERVERS, serverName));
+    } catch (e) {
+        return reportErr("create the server folder", e);
+    }
+    DIR.SERVER = path.join(DIR.SERVERS, serverName);
+
+    try {
+        fs.writeFileSync(path.join(DIR.SERVER, "./eula.txt"), "eula=true");
+    } catch (e) {
+        DIR.SERVER = null;
+        return reportErr("accepting the EULA", e);
+    }
+    try {
+        fs.writeFileSync(path.join(DIR.SERVER, "./start.bat"), "java -Xms512M -Xmx2G -jar server.jar nogui");
+    } catch (e) {
+        DIR.SERVER = null;
+        return reportErr("creating the start script", e);
+    }
+    try {
+        (async () => {
+            if (document.querySelector("#AddServer_Image").files.length > 0) {
+                const image = document.querySelector("#AddServer_Image").files[0];
+                fs.copyFileSync(image.path, path.join(DIR.SERVER, "./server-icon-0.png"));
+
+                let pathi = fs.readFileSync(path.join(DIR.SERVER, "./server-icon-0.png"));
+                const img2 = await resizeImg(Buffer(pathi), {
+                    width: 64,
+                    height: 64,
+                    format: "png",
+                });
+
+                fs.writeFileSync(path.join(DIR.SERVER, "server-icon.png"), img2)
+            }
+        })();
+    } catch (e) {
+        DIR.SERVER = null;
+        return reportErr("resizing the server image", e);
+    }
+    showPanel("ServerSelect");
 };
 
 const showPanel = (panel) => {
@@ -68,3 +129,11 @@ const switchDesc = () => {
 };
 switchDesc();
 const disableBtn = () => document.querySelector("#AddServer_CreateButton").disabled = !document.querySelector("#AddServer_EULA").checked;
+const reportErr = (tag, e) => {
+    if (dialog.showMessageBoxSync(null, {
+        type: "error",
+        title: "Minecraft Server Shell",
+        message: `An unexpected error occurred${tag === "" ? "" : ` while trying to ${tag}`}.\n\n${e}\n\nPlease report the bug at the GitHub repository.`,
+        buttons: ["Open GitHub Repository", "Close"],
+    }) === 0) openExternal("https://github.com/edwardscamera/MinecraftServerShell/issues");
+}
