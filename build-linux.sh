6b0a722e5faaddd3371a40534b4e4309843b363b
edwardscamera <ebleselrod@gmail.com>
2021-08-21 20:15:24 -0400

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,4 @@
 rm -r ./build/
 mkdir ./build/
 electron-packager ./ --out=./build/ --platform=linux --asar --icon=./src/img/icon.ico --arch=x64 --executableName=minecraftservershell
-electron-installer-debian --src build/MinecraftServerShell-linux-x64 --dest build/ --arch amd64
+electron-installer-debian --src "build/Minecraft Server Shell-linux-x64" --dest build/ --arch amd64
